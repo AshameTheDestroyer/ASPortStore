99e58a7dfb58da1f136569f4d35192a6512ae4d0
Hashem Wannous <hashemwnoos@gmail.com>
2024-01-18 10:14:44 +0200

    First Commit

---
+++ ASPortStore/Controllers/HomeController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index() => View();
+}
---
+++ ASPortStore/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+namespace SportsStore.Models;
+
+public class Product
+{
+    public long? ProductID { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    [Column(TypeName = "decimal(8, 2)")]
+    public decimal Price { get; set; }
+    public string Category { get; set; } = string.Empty;
+}
